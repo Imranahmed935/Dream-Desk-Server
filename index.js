8d602d44273578e01265ae442e414192d0808ce9
Imranahmed935 <imrantahir9918@gmail.com>
2024-12-17 02:13:22 -0800

    done

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-require('dotenv').config()
-const express =  require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://new-firebase-project-db686.web.app",
+      "https://new-firebase-project-db686.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
 
+app.use(express.json());
+app.use(cookieParser());
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.USER_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.haqk7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,28 +43,126 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.connect();
+
+    const jobsCollection = client.db("dream_desk").collection("jobs");
+    const jobApplicationStore = client
+      .db("dream_desk")
+      .collection("jobs-Application");
+
+    app.get("/jobs", async (req, res) => {
+      const email = req.query.email;
+      let query = {};
+      if (email) {
+        query = { hr_email: email };
+      }
+      const cursor = jobsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.USER_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    app.post("/jobs", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollection.insertOne(newJob);
+      res.send(result);
+    });
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const data = await jobsCollection.findOne(query);
+      res.send(data);
+    });
+
+    app.get("/jobs-application", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const query = { user_email: email };
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+      // if(req.user.email !== req.query.email){
+      //   return res.status(403).send({message:'forbidden access'})
+      // }
+      const result = await jobApplicationStore.find(query).toArray();
+      for (const application of result) {
+        const query1 = { _id: new ObjectId(application.job_id) };
+        const job = await jobsCollection.findOne(query1);
+        if (job) {
+          (application.title = job.title), (application.company = job.company);
+        }
+      }
+      res.send(result);
+    });
+
+    app.get("/jobs-applications/jobs/:job_id", async (req, res) => {
+      const jobId = req.params.job_id;
+      const query = { job_id: jobId };
+      const result = await jobApplicationStore.find(query).toArray();
+      res.send(result);
+    });
+
+    app.patch("/jobs-applications/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: data.status,
+        },
+      };
+      const result = await jobApplicationStore.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    app.post("/jobs-applications", async (req, res) => {
+      const application = req.body;
+      const result = await jobApplicationStore.insertOne(application);
+      res.send(result);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("the Dream Desk server is Running.");
+});
 
-app.get('/', (req, res)=>{
-    res.send('the Dream Desk server is Running.')
-})
-
-app.listen(port, ()=>{
-    console.log(`the server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`the server is running on port ${port}`);
+});
